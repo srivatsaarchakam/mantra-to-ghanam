4df2d8af6e53eb62027df77eaf40741fd8e949c6
Srivatsa Archakam <srivatsa.s.krishnamurthy@gmail.com>
2025-02-08 17:48:11 -0500

    link reference change

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -55,7 +55,7 @@ export default function HomePage() {
       setGhanam("");
 
       // Adjust URL if needed
-      const response = await fetch("http://127.0.0.1:5000/transform", {
+      const response = await fetch("https://mantra-to-ghanam.onrender.com/transform", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ mantra }),
